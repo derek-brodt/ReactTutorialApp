cd070ab39461dde75c1dae60e0aa2fde0c34fd2b
Derek Dreibrodt <ddreibrodt1@gmail.com>
2022-07-30 00:59:59 -0500

    Completed the main tic-tac-toe game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+// The Squares are "controlled components", the board has full control over them
+function Square(props) {
       // We can't modify the parent's state so we have
       // to pass in a function from parent (Board) to child (Square)
       // to call onClick()
       return (
-        <button 
-        className="square" 
-        onClick={() =>
+        <button className="square" onClick={() =>
             // The function passed in as a prop from Board.renderSquare(i)
-            this.props.onClick()
+            props.onClick()
         }>
-          {this.props.value}
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
@@ -30,16 +27,31 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares:Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i) {
       // Const is the array structure, not the values within it
+      // We maintain immutability because:
+      // 1. Complex features such as a history of moves becomes easier to implement
+      // 2. Detecting changesi n mutable objects is difficult since they are modified directly.
+      //    This requires the mutable object to be compared to previous copies of itself
+      //    and the entire object tree must be traversed. Immutable objects are easier;
+      //    to detect immutable changes, check if the object referenced is different than the previous one
+      // 3. Detecting changes determines when components must be re-rendered https://reactjs.org/docs/optimizing-performance.html#examples
       const const_squares = this.state.squares.slice();
+      if (calculateWinner(const_squares) || const_squares[i]) {
+        return;
+      }
       // Change the constant's values
-      const_squares[i] = "X";
+      const_squares[i] = this.state.xIsNext ? "X" : "O";
       // Changes the squares state to the updated values of the squares
-      this.setState({squares: const_squares})
+      // States must be set by this.setState
+      this.setState({
+        squares: const_squares,
+        xIsNext: !this.state.xIsNext
+      })
     }
 
     renderSquare(i) {
@@ -52,7 +64,15 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      // If winner is X or O, it returns True; if null, evaluates to false
+      if (winner) {
+        status = "Winner " + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+      }
+      
   
       return (
         <div>
@@ -91,7 +111,26 @@ class Square extends React.Component {
         </div>
       );
     }
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
+}  
   
   // ========================================
   
